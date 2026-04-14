d1cbe01c94a729b342ca1b62859694316836bfef
Charlotte Leger <charlotteleger@Mac-001.lan>
2026-04-15 00:52:34 +0200

    resolved link html

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        work: resolve(__dirname, 'src/Works.html'),
+        works: resolve(__dirname, 'src/works.html'),
         about: resolve(__dirname, 'src/About.html'),
         sketchbook: resolve(__dirname, 'src/projectPages/Sketchbook.html'),
         biodiversite: resolve(__dirname, 'src/projectPages/Biodiversite.html'),
