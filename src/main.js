3435e78ae5aa6d1c45a5d9bd2f850ca6ba562b4e
Charlotte Leger <charlotteleger@Mac-001.lan>
2026-04-15 21:55:07 +0200

    test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { gsap } from 'https://cdn.skypack.dev/gsap';
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import "../assets/CV-Charlotte-Leger-2026.pdf";
+import cvPath from './assets/CV-Charlotte-Leger-2026.pdf'
+
+document.getElementById('cv-link').href = cvPath
 
 // On attend que le DOM soit chargé
 document.addEventListener("DOMContentLoaded", () => {
